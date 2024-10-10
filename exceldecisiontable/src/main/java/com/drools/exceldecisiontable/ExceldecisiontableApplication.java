619cf3517f1a78340c30b5198544899074491b01
KUZIVAKWASHE  CHAWATAMA <kuzivakwashechawatama@gmail.com>
2024-10-10 13:05:30 +0200

    Add files via upload

---
+++ exceldecisiontable/src/main/java/com/drools/exceldecisiontable/ExceldecisiontableApplication.java
@@ -0,0 +1,13 @@
+package com.drools.exceldecisiontable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceldecisiontableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceldecisiontableApplication.class, args);
+	}
+
+}
